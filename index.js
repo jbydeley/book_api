98642d1288d064f3c7436db21422d3a8f1005ff5
Jared Bydeley <jared@bydeley.ca>
2020-01-15 23:43:46 -0500

    Update list of books

--- index.js
+++ index.js
@@ -6,16 +6,25 @@ const books = [
         id: 0,
         title: 'The Secrets They Keep',
         author: 'Alison Haines',
+        rating: 5,
     },
     {
         id: 1,
         title: 'Harry Potter and the Chamber of Secrets',
         author: 'J.K. Rowling',
+        rating: 4,
     },
     {
         id: 2,
         title: 'Jurassic Park',
         author: 'Michael Crichton',
+        rating: 3,
+    },
+    {
+        id: 3,
+        title: 'Mistborn',
+        author: 'Brandon Sanderson',
+        rating: 5,
     },
 ];
 
